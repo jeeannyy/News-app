a3b7708cb2ff9fcaaeed769c09a800c819e127bf
Jeeann <jeeann.kim@gmail.com>
2022-07-29 14:48:11 +0100

    edit

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -59,9 +59,9 @@ const MyAccount = () => {
           <option value="tickle122">tickle122</option>
           <option value="grumpy19">grumpy19</option>
           <option value="happyamy2016">happyamy2016</option>
-          <option value="happyamy2016">cooljmessy</option>
-          <option value="happyamy2016">weegembump</option>
-          <option value="happyamy2016">jessjelly</option>
+          <option value="cooljmessy">cooljmessy</option>
+          <option value="weegembump">weegembump</option>
+          <option value="jessjelly">jessjelly</option>
       </select>
       <button type="submit" className='loginBtn'>Login</button>
       </form>
